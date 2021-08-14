8c6c23fd5da4e0b457d9dcaa119b7479c003fbfb
한경동 <hkd0694@gmail.com>
2021-08-15 01:07:03 +0900

    Test

---
+++ 2st_11_ControlFlowWhile/app/src/main/java/com/example/controlflowwhile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.controlflowwhile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.controlflowwhile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
