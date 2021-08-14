be9ee68eadb2478c714cd5a484c5b6ef9d3860af
한경동 <hkd0694@gmail.com>
2021-08-15 01:23:58 +0900

    ControlFlowWhile Commit

--- 2st_11_ControlFlowWhile/app/src/main/java/com/example/controlflowwhile/MainActivity.kt
+++ 2st_11_ControlFlowWhile/app/src/main/java/com/example/controlflowwhile/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.controlflowwhile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.controlflowwhile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. 일반적인 while 사용하기
+        var current = 1
+        var until = 12
+        while( current < until ) {
+            Log.d("while","현재 값은 $current 입니다.")
+            // current 1씩 증가시켜서 열한 번 반복한 후 while 문을 빠져나갑니다.
+            current++
+        }
+
+        // 2. do ~ while 사용하기
+        var game = 1
+        val match = 6
+        do {
+            Log.d("while", "$game 게임 이겼습니다. 우승까지 ${match - game} 게임 남았습니다.")
+            game += 1
+        } while(game < match)
+
+        // 3. while vs do ~ while
+        // while 테스트
+        game = 6
+        while(game < match) {
+            Log.d("while", "**** while 테스트 입니다. ***")
+            game += 1
+        }
+
+        // do ~ while 테스트
+        game = 6
+        do {
+            Log.d("while", "*** do ~ while 테스트 입니다. ***")
+            game += 1
+        } while(game < match)
+
+        // 4. break 반복문 탈출하기
+        for( index in 1..10 ) {
+            Log.d("while", "break > 현재 index는 $index 입니다.")
+            if( index > 5 ) {
+                break
+            }
+        }
+
+        // 5. continue 다음 반복문으로
+        for(except in 1..10) {
+            if(except > 3 && except < 8) {
+                continue
+            }
+            Log.d("while", "continue > 현재 index는 $except 입니다.")
+        }
+
     }
 }
