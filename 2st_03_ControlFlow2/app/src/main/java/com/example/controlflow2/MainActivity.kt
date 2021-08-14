6b64ca848c7a5665fb4cbe9a82a036d39577f2d0
한경동 <hkd0694@gmail.com>
2021-08-14 11:19:32 +0900

    ControlFlow2 Commit

--- 2st_03_ControlFlow2/app/src/main/java/com/example/controlflow2/MainActivity.kt
+++ 2st_03_ControlFlow2/app/src/main/java/com/example/controlflow2/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.controlflow2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var a = 1
+        var b = 2
+        var c = 3
+
+        // if 문 두 번 사용하기
+        if(a < b) {
+            Log.d("ControlFlow2", "1: a는 b보다 작습니다.")
+        }
+        if(a < c) {
+            Log.d("ControlFlow2", "1: a는 c보다 작습니다.")
+        }
+
+        // 2. else if 문 사용하기
+        if(a < b) {
+            Log.d("ControlFlow", "2: a는 b보다 작습니다.")
+        } else if (a < c) {
+            Log.d("ControlFlow", "2: a는 c보다 작습니다.")
+        }
+
     }
 }
